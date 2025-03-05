7061829f0363995d65ceb48550694cda8683d27c
Afrizal Qurratul Faizin <afrizalquratulfaizih@gmail.com>
2025-03-05 10:03:37 +0700

    Pertemuan 4, Percobaan 2.5 - Attribute Changes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,16 +10,25 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::firstOrNew(
+        $user = UserModel::create(
             [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
+                'username' => 'manager11',
+                'nama' => 'Manager11',
                 'password' => Hash::make('12345'),
                 'level_id' => 2
             ]
         );
-        $user->save();
+
+        $user->username = 'manager12';
         
-        return view('user', ['data' => $user]);
+        $user->save();
+        $user->wasChanged();
+        $user->wasChanged('username');
+        $user->wasChanged(['username', 'level_id']);
+        $user->wasChanged('nama');
+        $user->wasChanged(['nama', 'username']);
+        dd($user->wasChanged(['nama', 'username']));
+
+        //return view('user', ['data' => $user]);
     }
 }
