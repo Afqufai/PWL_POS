960110b35722e269840a2c3baace1f28a27830d6
Afrizal Qurratul Faizin <afrizalquratulfaizih@gmail.com>
2025-03-17 16:57:59 +0700

    Pertemuan 6, Percobaan 2 - Edit Dengan Ajax

--- routes/web.php
+++ routes/web.php
@@ -34,8 +34,8 @@ Route::group(['prefix' => 'user'], function(){
     Route::get('/{id}/edit', [UserController::class, 'edit']);
     Route::put('/{id}', [UserController::class, 'update']);
 
-    //Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']);
-    //Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']);
+    Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']);
+    Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']);
 
     Route::delete('/{id}', [UserController::class, 'destroy']);
 });
