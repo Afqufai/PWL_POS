b19d32954126007091cd6f5f0155e0b1290e3efb
Afrizal Qurratul Faizin <afrizalquratulfaizih@gmail.com>
2025-05-02 16:15:20 +0700

    Pertemuan 10, Tugas - Implementasi CRUD dengan API

--- routes/api.php
+++ routes/api.php
@@ -31,3 +31,23 @@ Route::get('levels/{level}', [LevelController::class, 'show']);
 Route::put('levels/{level}', [LevelController::class, 'update']);
 Route::delete('levels/{level}', [LevelController::class, 'destroy']);
 
+//ROUTE USER
+Route::get('users', [\App\Http\Controllers\Api\UserController::class, 'index']);
+Route::post('users', [\App\Http\Controllers\Api\UserController::class, 'store']);
+Route::get('users/{user}', [\App\Http\Controllers\Api\UserController::class, 'show']);
+Route::put('users/{user}', [\App\Http\Controllers\Api\UserController::class, 'update']);
+Route::delete('users/{user}', [\App\Http\Controllers\Api\UserController::class, 'destroy']);
+
+//ROUTE BARANG
+Route::get('stuff', [\App\Http\Controllers\Api\BarangController::class, 'index']);
+Route::post('stuff', [\App\Http\Controllers\Api\BarangController::class, 'store']);
+Route::get('stuff/{barang}', [\App\Http\Controllers\Api\BarangController::class, 'show']);
+Route::put('stuff/{barang}', [\App\Http\Controllers\Api\BarangController::class, 'update']);
+Route::delete('stuff/{barang}', [\App\Http\Controllers\Api\BarangController::class, 'destroy']);
+
+//ROUTE KATEGORI
+Route::get('categories', [\App\Http\Controllers\Api\KategoriController::class, 'index']);
+Route::post('categories', [\App\Http\Controllers\Api\KategoriController::class, 'store']);
+Route::get('categories/{kategori}', [\App\Http\Controllers\Api\KategoriController::class, 'show']);
+Route::put('categories/{kategori}', [\App\Http\Controllers\Api\KategoriController::class, 'update']);
+Route::delete('categories/{kategori}', [\App\Http\Controllers\Api\KategoriController::class, 'destroy']);
